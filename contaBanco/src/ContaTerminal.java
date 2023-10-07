262e99a7701de17a03e98b3814741dc1b55c3cd4
Talissa <talissasar@gmail.com>
2023-10-07 05:11:21 -0300

    Commit inicial

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Digite o número da sua conta e aperte a tecla ENTER");
+      int conta = scanner.nextInt();
+
+      System.out.println("Digite o código da sua agência e aperte a tecla ENTER");
+      String agencia = scanner.next();
+
+      System.out.println("Escreva seu nome completo e aperte a tecla ENTER");
+      String nomeSobrenome = scanner.next();
+
+      System.out.println("Digite o saldo que deseja sacar e aperte a tecla ENTER");
+      double saldo = scanner.nextDouble();
+
+      System.out.println("Olá, " + nomeSobrenome + "! " + "Agradecemos por criar uma conta em nosso banco. " + "Sua agência é " + agencia + " e o número de sua conta é " + conta + ". " + "Seu saldo de " + "R$"+ saldo + " já está disponível para saque.");
     }
 }
